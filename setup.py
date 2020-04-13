f8a5a000daab45e72423f8f4c65c8bf1afe15e52
James Lan <jlan@magicleap.com>
2020-04-13 12:20:02 -0700

    Add coverage and static check

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ def main():
     setup(
         name='setuptools-cmd-deps',
         version='0.1.0',
-        description='setuptools plugin to configure the dependency among commands',
         long_description=open('README.md').read(),
         url='https://github.com/jameslan/setuptools-cmd-deps',
-        python_requires='>=3.5',
         py_modules=['setuptools_cmd_deps'],
     )
 
--- tests/test_cmd_deps.py
+++ tests/test_cmd_deps.py
@@ -5,26 +5,27 @@ from os.path import dirname
 project_dir = dirname(dirname(__file__))
 
 
-def test_implicit_dep():
-    out = check_output(
-        ['python', '-c', '__import__("setuptools").setup()', 'build_py'],
+def run_setup(code, command):
+    # run in project dir so that the entry_points settings
+    # in setup.cfg could take effect
+    return check_output(
+        ['python', '-c', code, command],
         cwd=project_dir,
     ).decode()
+
+
+def test_implicit_dep():
+    out = run_setup('__import__("setuptools").setup()', 'build_py')
     assert 'running decl_cmd1' in out
     assert 'running decl_cmd2' in out
     assert 'running fixture_custom_cmd' in out
 
 
 def test_setup_keyword():
-    out = check_output(
-        [
-            'python',
-            '-c',
-            '__import__("setuptools").setup(cmd_deps={"alias":["build_py"]})',
-            'alias',
-        ],
-        cwd=project_dir,
-    ).decode()
+    out = run_setup(
+        '__import__("setuptools").setup(cmd_deps={"alias":["build_py"]})',
+        'alias',
+    )
     assert 'running build_py' in out
     assert 'running decl_cmd1' in out
     assert 'running decl_cmd2' in out
